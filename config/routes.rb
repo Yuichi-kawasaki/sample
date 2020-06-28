ac5f1cdfcfadc0b5ec11dfe6c4abf3893e9bbb68
yuichi kawasaki <jiv.yu1.kawasaki@gmail.com>
2020-06-28 18:37:06 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get '/stocks', to: 'stocks#index'
-  root to: 'blogs#index'
-  get 'blogs/index'
-  get '/blogs', to: 'blogs#index'
+  
   resources :blogs do
     collection do
       post :confirm
